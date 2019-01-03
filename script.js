2c0da52c570308037420fe620c8dbedad67044c3
ブレンダ <43443759+dogbrenda2705@users.noreply.github.com>
2019-01-03 09:48:50 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$(".feather").click(function(){
+    $(".float").slideUp();
+ 
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
